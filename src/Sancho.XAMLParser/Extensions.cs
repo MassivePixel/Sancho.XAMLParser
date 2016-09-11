6f9327977d85bd3d2505c406bccfea5a5baf6276
Toni Petrina <tpetrina@tpetrina.com>
2016-09-11 09:20:17 +0200

    Added IfNotNull extension

--- src/Sancho.XAMLParser/Extensions.cs
+++ src/Sancho.XAMLParser/Extensions.cs
@@ -1,10 +1,19 @@
 // Copyright (c) Massive Pixel.  All Rights Reserved.  Licensed under the MIT License (MIT). See License.txt in the project root for license information.
 
+using System;
+
 namespace Sancho.XAMLParser
 {
     public static class Extensions
     {
         public static T As<T>(this object o) where T : class => o as T;
         public static T Cast<T>(this object o) => (T)o;
+
+        public static R IfNotNull<T, R>(this T o, Func<T, R> func)
+            where T : class
+        {
+            if (o == null) return default(R);
+            return func(o);
+        }
     }
 }
