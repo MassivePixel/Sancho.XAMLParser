5f8081d3c3ba2f29ccc07317f74f94323bbe9ec9
Toni Petrina <tpetrina@tpetrina.com>
2016-09-11 20:54:30 +0200

    Added bindable object helper for inspecting UI elements

---
+++ src/Sacho.DOM.XamarinForms/BindableObjectHelper.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (c) Massive Pixel.  All Rights Reserved.  Licensed under the MIT License (MIT). See License.txt in the project root for license information.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Xamarin.Forms;
+
+namespace Sancho.DOM.XamarinForms
+{
+    [Flags]
+    public enum BindableContextAttributes
+    {
+        IsManuallySet = 1 << 0,
+        IsBeingSet = 1 << 1,
+        IsDynamicResource = 1 << 2,
+        IsSetFromStyle = 1 << 3,
+        IsDefaultValue = 1 << 4
+    }
+
+    public class BindablePropertyContext
+    {
+        public BindableContextAttributes Attributes;
+        public BindingBase Binding;
+        public BindableProperty Property;
+        public object Value;
+    }
+
+    public static class BindableObjectHelper
+    {
+        public static List<BindablePropertyContext> GetProperties(this BindableObject bo)
+        {
+            if (bo == null)
+                throw new ArgumentNullException(nameof(bo));
+
+            var list = new List<BindablePropertyContext>();
+
+            var props = typeof(BindableObject)
+                .GetTypeInfo()
+                .GetDeclaredField("_properties")
+                .GetValue(bo)
+                as IEnumerable;
+
+            if (props != null)
+            {
+                Type propType = null;
+
+                foreach (var prop in props)
+                {
+                    propType = propType ?? prop.GetType();
+                    var p = propType.GetRuntimeField("Property")?.GetValue(prop) as BindableProperty;
+                    var v = propType.GetRuntimeField("Value")?.GetValue(prop);
+                    var b = propType.GetRuntimeField("Binding")?.GetValue(prop) as BindingBase;
+                    var a = (int)propType.GetRuntimeField("Attributes")?.GetValue(prop);
+                    list.Add(new BindablePropertyContext
+                    {
+                        Property = p,
+                        Value = v,
+                        Binding = b,
+                        Attributes = (BindableContextAttributes)a
+                    });
+                }
+            }
+
+            return list;
+        }
+    }
+}
---
+++ tests/ParserTests/ReflectionTests.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Massive Pixel.  All Rights Reserved.  Licensed under the MIT License (MIT). See License.txt in the project root for license information.
+
+using System.Collections;
+using System.Linq;
+using System.Reflection;
+using Sancho.DOM.XamarinForms;
+using Xamarin.Forms;
+using Xunit;
+
+namespace XamarinFormsTests
+{
+    public class ReflectionTests
+    {
+        [Fact]
+        public void TestInternalProperties()
+        {
+            var style = new Style(typeof(Label));
+            style.Setters.Add(Label.TextColorProperty, Color.Red);
+
+            var label = new Label
+            {
+                Margin = new Thickness(1),
+                Style = style
+            };
+            label.SetBinding(Label.TextProperty, new Binding("Name"));
+
+            var fields = typeof(BindableObject).GetFields(BindingFlags.NonPublic | BindingFlags.Instance);
+            var propsField = fields.FirstOrDefault(x => x.Name == "_properties");
+
+            Assert.NotNull(propsField);
+
+            var props = propsField.GetValue(label) as IEnumerable;
+            Assert.NotNull(props);
+
+            foreach (var prop in props)
+            {
+                var p = prop.GetType().GetField("Property").GetValue(prop);
+                var v = prop.GetType().GetField("Value").GetValue(prop);
+            }
+
+            var props2 = BindableObjectHelper.GetProperties(label);
+        }
+    }
+}
+
